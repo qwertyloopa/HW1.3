ca5a7b4605b9de1aea1adb72ad6e6fb47fdf4188
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-11-27 00:33:39 +0300

    сделаны все задания

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine(default(int));
+            Console.WriteLine(default(float));
+            Console.WriteLine(default(byte));
+            Console.WriteLine(default(sbyte));
+            Console.WriteLine(default(double));
+            Console.WriteLine(default(decimal));
+            Console.WriteLine(default(uint));
+            Console.WriteLine(default(ulong));
+            Console.WriteLine(default(ushort));
             Console.WriteLine(default(string));
             Console.WriteLine(default(char));
             Console.WriteLine(default(bool));
---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int MyHeight = 179;
+            Console.WriteLine($"{nameof(MyHeight)} is {MyHeight}");
+            Console.ReadKey();
+        }
+    }
+}
