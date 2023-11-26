b3a7507ce3ba91c82ec8e9c42b4576a6143ba16f
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-11-26 19:12:51 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = Math.PI;
+            Console.WriteLine("Введите радиус");
+            int r = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите образующую");
+            int I = Int32.Parse(Console.ReadLine());
+            double s = pi * r * (r + I);
+            Console.WriteLine($"Площадь круга равна {s}");
+        }
+    }
+}
